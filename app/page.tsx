93f59b6a515fc262978d9b39d056fac2346ed802
mohamedfahim1045 <mohamedfahim1045@gmail.com>
2025-01-08 17:48:04 +0200

    solve some next problems in deployment

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import SetRegion from "./setregion/page";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
-export const metaData: Metadata = {
+export const metadata: Metadata = {
     title: 'Select Your Current Region',
     description: 'Select your current region to get personalized recommendations and offers.',
 };
@@ -13,7 +13,7 @@ export default async function MainHome() {
     const region: string | undefined = cookiesData.get('region')?.value;
 
     if (region) {
-        redirect(`/${region}/discover/home`);
+        return redirect(`/${region}/discover/home`);
     } else {
         return <SetRegion />
     };
