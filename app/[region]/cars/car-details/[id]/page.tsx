6872ef03fbc605743e066efd2fa70646bad79f57
MohamedFahim123 <https://github.com/MohamedFahim123>
2025-01-18 14:33:02 +0200

    solve deployment error1

--- app/[region]/cars/car-details/[id]/page.tsx
+++ app/[region]/cars/car-details/[id]/page.tsx
@@ -5,8 +5,8 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Car Details",
   description: "",
-};
-export default function page({ params }: { params: { id: number } }) {
-  const carItem = allCars.filter((elm) => elm.id == params.id)[0] || allCars[0];
+}; 
+export default function page({ params }: { params: { id: string } }) {
+  const carItem = allCars.filter((elm) => +elm.id === +params.id)[0] || allCars[0];
   return <CarDetailsSection carItem={carItem}/>;
 }
