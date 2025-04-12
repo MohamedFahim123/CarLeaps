e9e859a7e8eef05dbb9f8d231e95e026edcd356d
MohamedFahim123 <https://github.com/MohamedFahim123>
2025-04-12 17:41:06 +0200

    integrate cpo cars and filter it with bodies

--- app/store/CPOCars.ts
+++ app/store/CPOCars.ts
@@ -6,8 +6,14 @@ import { baseUrl, MainRegionName } from "../utils/mainData";
 import { Car } from "./CarsForSale";
 import { useResearchCarsMakesStore } from "./ResearchCarMakes";
 
+export interface CPOCarsInterface {
+  body_id: number;
+  body_name: string;
+  cars: Car[];
+}
+
 export interface UseCPOCarsStoreInterface {
-  CPOCars: Car[];
+  CPOCars: CPOCarsInterface[];
   CPOCarsError: unknown;
   CPOCarsLoading: boolean;
   hasFetchError: boolean;
@@ -17,70 +23,66 @@ export interface UseCPOCarsStoreInterface {
 let lastFetchedTime: number = 0;
 const CACHE_EXPIRATION_TIME: number = 15 * 60 * 1000;
 
-export const useCPOCarsStore =
-  create<UseCPOCarsStoreInterface>((set, get) => ({
-    CPOCars: [],
-    CPOCarsError: null,
-    CPOCarsLoading: false,
-    hasFetchError: false,
+export const useCPOCarsStore = create<UseCPOCarsStoreInterface>((set, get) => ({
+  CPOCars: [],
+  CPOCarsError: null,
+  CPOCarsLoading: false,
+  hasFetchError: false,
 
-    getCPOCars: async () => {
-      const { currRegion } = useResearchCarsMakesStore.getState();
-      const currentTime: number = new Date().getTime();
+  getCPOCars: async () => {
+    const { currRegion } = useResearchCarsMakesStore.getState();
+    const currentTime: number = new Date().getTime();
 
-      if (!currRegion) {
-        Cookies.set("region", MainRegionName);
+    if (!currRegion) {
+      Cookies.set("region", MainRegionName);
+    }
+
+    if (get().hasFetchError) {
+      console.warn("Skipping fetch due to previous error.");
+      return;
+    }
+
+    if (
+      lastFetchedTime !== 0 &&
+      currentTime - lastFetchedTime < CACHE_EXPIRATION_TIME
+    ) {
+      return;
+    }
+
+    set({ CPOCarsLoading: true });
+
+    try {
+      const res = await axios.get(`${baseUrl}/cpo-cars?t=${currentTime}`, {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          code: currRegion || MainRegionName,
+        },
+      });
+
+      const CPOCarsCars = res?.data?.data || [];
+
+      lastFetchedTime = currentTime;
+
+      set({
+        CPOCars: CPOCarsCars,
+        CPOCarsError: null,
+        CPOCarsLoading: false,
+        hasFetchError: false,
+      });
+    } catch (err) {
+      set({
+        CPOCars: [],
+        CPOCarsError: axios.isAxiosError(err)
+          ? err?.response?.data?.message
+          : "Error fetching CPOCars",
+        CPOCarsLoading: false,
+        hasFetchError: true,
+      });
+
+      if (!axios.isAxiosError(err)) {
+        toast.error("Unexpected error occurred!");
       }
-
-      if (get().hasFetchError) {
-        console.warn("Skipping fetch due to previous error.");
-        return;
-      }
-
-      if (
-        lastFetchedTime !== 0 &&
-        currentTime - lastFetchedTime < CACHE_EXPIRATION_TIME
-      ) {
-        return;
-      }
-
-      set({ CPOCarsLoading: true });
-
-      try {
-        const res = await axios.get(
-          `${baseUrl}/cpo-cars?t=${currentTime}`,
-          {
-            headers: {
-              "Content-Type": "application/json",
-              Accept: "application/json",
-              code: currRegion || MainRegionName,
-            },
-          }
-        );
-
-        const CPOCarsCars = res?.data?.data?.cars || [];
-
-        lastFetchedTime = currentTime;
-
-        set({
-          CPOCars: CPOCarsCars,
-          CPOCarsError: null,
-          CPOCarsLoading: false,
-          hasFetchError: false,
-        });
-      } catch (err) {
-        set({
-          CPOCars: [],
-          CPOCarsError: axios.isAxiosError(err)
-            ? err?.response?.data?.message
-            : "Error fetching CPOCars",
-          CPOCarsLoading: false,
-          hasFetchError: true,
-        });
-
-        if (!axios.isAxiosError(err)) {
-          toast.error("Unexpected error occurred!");
-        }
-      }
-    },
-  }));
+    }
+  },
+}));
